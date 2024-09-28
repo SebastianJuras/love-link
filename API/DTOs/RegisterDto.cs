49659af6f830ca0b6f587d9c6113feadbefc50d0
Sebastian <sebastian.juras@gmail.com>
2024-09-28 23:48:23 +0200

    Register DTO

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,8 +6,9 @@ public class RegisterDto
 {
     [Required]
     [MaxLength(100)]
-    public required string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
 
     [Required]
-    public required string Password { get; set; }
+    [MaxLength(8)]
+    public string Password { get; set; } = string.Empty;
 }
